9bdf423cf3ad1f26e65547cb7193b2ccfbfe2302
LeoF-07 <fortin.leonardo07@gmail.com>
2024-11-26 16:39:31 +0100

    Prima Versione

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        ok facciamo una prova
+                leggi negro?
+    }
+}
